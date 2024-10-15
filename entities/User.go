800d5d38394b31ca3412769e23624640acd0ca92
João Gustavo Soares Bispo <joaogustavobispo15@gmail.com>
2024-10-14 23:57:38 -0300

    User in database
    Criando todos os controllers responsáveis pela criação do usuario no sistema, ainda não foi completo, mas ta a caminho

--- entities/User.go
+++ entities/User.go
@@ -1,1 +1,25 @@
 package entities
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID        string    `json:"id"`
+	Name      string    `json:"name"`
+	Phone     string    `json:"phone"`
+	Mail      string    `json:"mail"`
+	CPF       string    `json:"cpf"`
+	CreatedAt time.Time `json:"createAt"`
+}
+
+func NewUser() *User {
+	User := User{
+		ID:        uuid.NewString(),
+		CreatedAt: time.Now().UTC(),
+	}
+
+	return &User
+}
--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	routes "github.com/jgb27/nidus-controller-projects/routes"
 )
 
 func main() {
 	app := gin.Default()
+
+	routes.Routes(app)
 
 	app.Run(":8080")
 }
