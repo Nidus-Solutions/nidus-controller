514679d60f9d724be5649678f8c16f09575cc7f6
João Gustavo Soares Bispo <joaogustavobispo15@gmail.com>
2024-10-22 14:40:21 -0300

    Upload and Edit to S3
    Tudo certo com o upload para s3

--- controllers/ProjectController.go
+++ controllers/ProjectController.go
@@ -18,14 +18,22 @@ var (
 	ENV             = services.LoadEnv("ENV")
 )
 
-func NewProject(ctx *gin.Context) {
-	var user models.User
-	var project = models.NewProject()
+func uploadDocument(ctx *gin.Context, project *models.Project) {
 	form, _ := ctx.MultipartForm()
 	files := form.File["files"]
 
 	for _, file := range files {
 		var Documents = models.NewDocument()
+
+		if file.Size > 1000000 {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Arquivo muito grande"})
+			return
+		}
+
+		if file.Header.Get("Content-Type") != "application/pdf" {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Arquivo não é um PDF"})
+			return
+		}
 
 		if err := services.Upload(file); err != nil {
 			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Erro ao fazer upload do arquivo"})
@@ -37,6 +45,13 @@ func NewProject(ctx *gin.Context) {
 		database.DB.Create(&Documents)
 		project.Documents = append(project.Documents, *Documents)
 	}
+}
+
+func NewProject(ctx *gin.Context) {
+	var user models.User
+	var project = models.NewProject()
+
+	uploadDocument(ctx, project)
 
 	if err := database.DB.Where("id = ?", ctx.PostForm("userId")).First(&user).Error; err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Usuário não encontrado"})
@@ -68,43 +83,69 @@ func NewProject(ctx *gin.Context) {
 
 func EditProject(ctx *gin.Context) {
 	id := ctx.Param("id")
+	var documents []models.Document
+	var project models.Project
+	form, _ := ctx.MultipartForm()
+	files := form.File["files"]
 
 	if id == "" {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": "ID nao encontrado"})
 		return
 	}
 
-	var project models.Project
+	database.DB.Where("project_id = ?", id).Find(&documents)
+
+	for _, doc := range documents {
+		for _, file := range files {
+			if doc.Name == file.Filename {
+				ctx.JSON(http.StatusBadRequest, gin.H{"error": "Arquivo já existe"})
+				return
+			}
+		}
+	}
 
 	if err := database.DB.Where("id = ?", id).First(&project).Error; err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Projeto não encontrado"})
 		return
 	}
 
-	if ctx.PostForm("file") != "" {
-		var document []models.Document
-
-		database.DB.Where("project_id = ?", id).Find(&document)
-
-		log.Println(document)
+	if len(files) > 0 {
+		uploadDocument(ctx, &project)
 	}
 
-	value, err := strconv.ParseFloat(ctx.PostForm("value"), 64)
+	database.DB.Where("user_id = ?", id).Preload("Documents").Find(&project)
 
-	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Valor inválido"})
+	if ctx.PostForm("name") != "" {
+		project.Name = ctx.PostForm("name")
+	}
+
+	if ctx.PostForm("company") != "" {
+		project.Company = ctx.PostForm("company")
+	}
+
+	if ctx.PostForm("cnpj") != "" {
+		project.CNPJ = ctx.PostForm("cnpj")
+	}
+
+	if ctx.PostForm("value") != "" {
+		value, err := strconv.ParseFloat(ctx.PostForm("value"), 64)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Valor inválido"})
+			return
+		}
+		project.Value = value
+	}
+
+	if ctx.PostForm("deadline") != "" {
+		project.Deadline = ctx.PostForm("deadline")
+	}
+
+	if err := database.DB.Save(&project).Error; err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Erro ao se comunicar com o DB"})
 		return
 	}
 
-	database.DB.Model(&project).Updates(models.Project{
-		Name:     ctx.PostForm("name"),
-		Company:  ctx.PostForm("company"),
-		CNPJ:     ctx.PostForm("cnpj"),
-		Deadline: ctx.PostForm("deadline"),
-		Value:    value,
-	})
-
-	ctx.JSON(http.StatusOK, project)
+	ctx.JSON(http.StatusOK, gin.H{"message": "Projeto atualizado com sucesso", "project": project})
 }
 
 func DeleteProject(ctx *gin.Context) {
